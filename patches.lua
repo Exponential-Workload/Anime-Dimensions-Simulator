c087e0aff663dfea01781b99a39760b597599425
YieldingCoder <90570076+YieldingExploiter@users.noreply.github.com>
2022-06-28 01:00:23 +0200

    fix: Patch for Fluxus

--- patches.lua
+++ patches.lua
@@ -1,5 +1,5 @@
 -- Fluxus
-if identifyexecutor and identifyexecutor() == 'Fluxus' then
+if identifyexecutor and  (string.lower(identifyexecutor()) == 'wrd-api' or identifyexecutor() == 'Fluxus') then
   -- Patch rconsolecreate
   local cc = rconsolecreate;
   getgenv().rconsolecreate=function()return cc('')end
