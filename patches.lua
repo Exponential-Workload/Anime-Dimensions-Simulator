ea4c77db9dff9f47c86f91df7ccf830bfb296817
YieldingCoder <90570076+YieldingExploiter@users.noreply.github.com>
2022-06-28 01:00:56 +0200

    fix: Dont break existing scripts

--- patches.lua
+++ patches.lua
@@ -2,5 +2,5 @@
 if identifyexecutor and  (string.lower(identifyexecutor()) == 'wrd-api' or identifyexecutor() == 'Fluxus') then
   -- Patch rconsolecreate
   local cc = rconsolecreate;
-  getgenv().rconsolecreate=function()return cc('')end
+  getgenv().rconsolecreate=function(...)return #{...} > 0 and cc(...) or cc('')end
 end
